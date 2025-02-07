3faeec7d2213a7fac22d4065e0a85ce00164bf90
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-02-07 14:12:36 +0400

    Update Program.cs

--- Project-API/Schedule/Program.cs
+++ Project-API/Schedule/Program.cs
@@ -59,10 +59,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseHttpsRedirection();
-}
+app.UseHttpsRedirection();
 
 app.UseAuthentication();
 

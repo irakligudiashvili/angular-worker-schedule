feed6c3edab9f0c6aa1d31c1ca171eff199d2111
Irakli Gudiashvili <igudiashvili99@gmail.com>
2025-02-07 13:41:12 +0400

    adjusted https redirection

--- Project-API/Schedule/Program.cs
+++ Project-API/Schedule/Program.cs
@@ -59,7 +59,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthentication();
 
